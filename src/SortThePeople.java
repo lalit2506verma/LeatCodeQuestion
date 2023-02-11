a57ce337eab0de355668a787736f7054665aa36a
Lalit Kumar Verma <lalit2506verma@gmail.com>
2023-02-11 21:40:54 +0530

    Commit - Instead of Hashtable used hashmap -- Solved

--- src/SortThePeople.java
+++ src/SortThePeople.java
@@ -5,37 +5,25 @@ public class SortThePeople {
         String[] names = {"Mary","John","Emma"};
         int[] heights = {180,165,170};
         String[] result = sortPeople(names, heights);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+        for (String s : result) {
+            System.out.println(s);
         }
     }
 
-
-    static String[] sortPeople(String[] names, int[] heights){
-
-        //creating the hash table to map the names to their height
-        Hashtable<Integer, String> table = new Hashtable<Integer, String>();
-        int length = heights.length;
-        for(int i = 0; i < length; i++){
-            table.put(heights[i], names[i]);
-        }
-
-        //now sorting the array of heights
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length - i - 1; j++) {
-                if(heights[j] <  heights[j + 1]){
-                    int temp = heights[j];
-                    heights[j] = heights[j + 1];
-                    heights[j + 1] = temp;
-                }
+    static String[] sortPeople(String[] names, int[] heights) {
+        Map<Integer, String> map = new HashMap<>();
+        String[] sortedNames = new String[heights.length];
+        int i = 0;
+        while (map.size() < heights.length) {
+            if (!map.containsKey(heights[i])) {
+                map.put(heights[i], names[i]);
             }
+            i++;
         }
-        String[] sortedNames = new String[length];
-        //now our array -> heights is sorted
-        // So we iterrate names according to height
-        for(int i = 0; i < length; i++){
-            String name = table.get(i);
-            sortedNames[i] = name;
+
+        Arrays.sort(heights);
+        for(int j=heights.length-1;j>=0;j--){
+            sortedNames[j] = map.get(heights[heights.length-j-1]);
         }
         return sortedNames;
     }
